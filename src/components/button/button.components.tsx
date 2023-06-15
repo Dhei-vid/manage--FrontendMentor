222e9c347f3b03db27d3484e9a7301058c443198
David Etuk <djetuk@gmail.com>
2023-06-15 23:09:24 +0100

    feat: built reusable button component

--- src/components/button/button.components.tsx
+++ src/components/button/button.components.tsx
@@ -1,7 +1,32 @@
-export type ButtonType = {
-  primary: string;
+import { ReactNode } from "react";
+
+// CREATED REUSEABLE COMPONENT FOR THIS BUTTON - I AM PROUD!
+enum ButtonType {
+  base,
+  inverted,
+}
+
+const BUTTON_STYLES: Record<ButtonType, string> = {
+  [ButtonType.base]: "bg-[#f25f3a] text-[#fafafa]",
+  [ButtonType.inverted]: "bg-[#fafafa] text-[#f25f3a]",
 };
 
-const Button = () => {};
+export type ButtonProps = {
+  children?: ReactNode;
+  buttonEle: ButtonType;
+};
+
+const Button = (props: ButtonProps) => {
+  const { children, buttonEle } = props;
+  const basicButton = `py-3 px-8 border-solid rounded-full`;
+  const finalButtonClasses = `${basicButton} ${BUTTON_STYLES[buttonEle]}`;
+  return <button className={finalButtonClasses}>{children}</button>;
+};
 
 export default Button;
+
+Button.defaultProps = {
+  buttonEle: ButtonType.base,
+};
+
+Button.buttonEle = ButtonType;
